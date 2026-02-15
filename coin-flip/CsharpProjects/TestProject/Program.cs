29e3ebcd22c6611e49a05fbadf00e8fd5a2e3308
Jeremy Opriasa <opriasajbusiness@gmail.com>
2026-02-15 15:33:37 +0800

    feat: coin-flip

---
+++ coin-flip/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+// Generates a random number between 1 and 2 to simulate a coin flip
+Random random = new Random();
+int randomFlip = random.Next(1, 3); // generates between 1 and 2
+
+Console.WriteLine($"Coin flip result: {(randomFlip == 1 ? "Heads" : "Tails")}");

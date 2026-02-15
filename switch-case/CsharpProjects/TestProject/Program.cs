ebc69e909fd7a4a77ad561e0c0fd6d5c21c51aa9
Jeremy Opriasa <opriasajbusiness@gmail.com>
2026-02-15 20:53:13 +0800

    feat: switch-case

---
+++ switch-case/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,53 @@
+﻿
+/* 
+
+Code challenge: rewrite if-elseif-else using a switch statement
+You'll start with code that uses an if-elseif-else construct to evaluate the components of a product SKU. The SKU (Stock Keeping Unit) is formatted using three coded values: <product #>-<2-letter color code>-<size code>. For example, a SKU value of 01-MN-L corresponds to (sweat shirt)-(maroon)-(large), and the code outputs a description that appears as "Product: Large Maroon Sweat shirt".
+
+*/
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+for (int i = 0; i < product.Length; i++)
+{
+    switch (i)
+    {
+        case 0: // Product type
+            type = product[i] switch
+            {
+                "01" => "Sweat shirt",
+                "02" => "T-Shirt",
+                "03" => "Sweat pants",
+                _ => "Other"
+            };
+            break;
+        
+        case 1: // Color
+            color = product[i] switch
+            {
+                "BL" => "Black",
+                "MN" => "Maroon",
+                _ => "White"
+            };
+            break;
+        
+        case 2: // Size
+            size = product[i] switch
+            {
+                "S" => "Small",
+                "M" => "Medium",
+                "L" => "Large",
+                _ => "One Size Fits All"
+            };
+            break;
+    }
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
---
+++ switch-case/README.md
@@ -0,0 +1,10 @@
+# switch-case
+
+A switch statement is a C# selection statement that provides an alternative to an if-elseif-else branching construct. The switch statement provides advantages over an if-elseif-else construct when evaluating a single value against a list of known matching values.
+
+Consider the following scenario:
+
+- You're working on an application related to food nutrition. A section of the code deals with fruits.
+- Your code includes a variable named fruit that's used to hold the name of different types of fruit.
+- You have a list of the 20 fruits that your application is focused on.
+- You want to branch your code based on the value assigned to fruit.

a96babf70da4e05982289b8dec6cd550d3f87907
Jeremy Opriasa <opriasajbusiness@gmail.com>
2026-02-15 15:46:42 +0800

    feat: role-based-permissions

---
+++ role-based-permissions/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Random random = new Random();
+int level = random.Next(1, 56);
+int role = random.Next(1, 3);
+string permission = role == 1 ? "Admin" : "Manager";
+
+Console.WriteLine($"Somebody with {permission} permissions and level {level} is trying to access the system.");
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
---
+++ role-based-permissions/README.md
@@ -0,0 +1,5 @@
+# role-based-permissions
+
+- Decision logic challenge
+In this challenge, you implement decision logic based on a series of business rules. The business rules specify the access that is granted to users based on their role-based permissions and their career level. Code branches display a different message to the user depending on their permissions and level.
+
